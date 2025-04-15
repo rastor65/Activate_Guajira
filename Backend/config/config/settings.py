174a4a4e1e23da78b1fa62d6bc3386abec45e01b
rastor65 <rdamianquintero@uniguajira.edu.co>
2025-04-15 11:20:24 -0500

    .

--- Backend/config/config/settings.py
+++ Backend/config/config/settings.py
@@ -48,11 +48,12 @@ VAPID_CLAIMS = {
     "sub": "mailto:rdamianquintero@uniguajira.edu.co"
 }
 
-CORS_ALLOW_ALL_ORIGINS = True
 CORS_ALLOWED_ORIGINS = [
-    "http://localhost:4200",  # ajusta si usas otro puerto
+    "http://localhost:4200", 
+     "https://activateguajira.up.railway.app"
 ]
-CORS_ORIGIN_ALLOW_ALL = True
+#CORS_ORIGIN_ALLOW_ALL = True
+
 CORS_ALLOW_CREDENTIALS = True
 
 # Application definition
