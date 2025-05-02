2cd9ae0856fc0a6be60556a5bc7d598017ac2712
rastor65 <rdamianquintero@uniguajira.edu.co>
2025-05-02 09:38:36 -0500

    .

--- Backend/config/config/settings.py
+++ Backend/config/config/settings.py
@@ -32,7 +32,13 @@ OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
 
 # ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
 import os
-ALLOWED_HOSTS = os.getenv("DJANGO_ALLOWED_HOSTS", "*").split(",")
+
+ALLOWED_HOSTS = [
+    'localhost',
+    '127.0.0.1',
+    'activate-guajira.up.railway.app'
+]
+
 
 CELERY_BROKER_URL = 'redis://localhost:6379/0'
 
